1d19d6a93337dd14347f9e8b197ca27afaf883bf
Lihuen Heredia <lihuenh@gmail.com>
2021-05-10 12:25:44 -0300

    primer commit

---
+++ TP4_SIM/TestMontecarlo.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace TP4_SIM_vLihuen
+{
+    partial class TestMontecarlo
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grp_parametros = new System.Windows.Forms.GroupBox();
+            this.lbl_desde = new System.Windows.Forms.Label();
+            this.lbl_hasta = new System.Windows.Forms.Label();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.txt_desde = new System.Windows.Forms.TextBox();
+            this.txt_hasta = new System.Windows.Forms.TextBox();
+            this.btn_generar_simulacion = new System.Windows.Forms.Button();
+            this.txt_media = new System.Windows.Forms.TextBox();
+            this.lbl_media = new System.Windows.Forms.Label();
+            this.txt_cantidad_iteraciones = new System.Windows.Forms.TextBox();
+            this.lbl_cantidad_iteraciones = new System.Windows.Forms.Label();
+            this.grp_datos = new System.Windows.Forms.GroupBox();
+            this.dgv_resultados = new System.Windows.Forms.DataGridView();
+            this.grp_resultados = new System.Windows.Forms.GroupBox();
+            this.txt_toneladas_totales = new System.Windows.Forms.TextBox();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.grp_parametros.SuspendLayout();
+            this.grp_datos.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_resultados)).BeginInit();
+            this.grp_resultados.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grp_parametros
+            // 
+            this.grp_parametros.Controls.Add(this.lbl_desde);
+            this.grp_parametros.Controls.Add(this.lbl_hasta);
+            this.grp_parametros.Controls.Add(this.btn_limpiar);
+            this.grp_parametros.Controls.Add(this.txt_desde);
+            this.grp_parametros.Controls.Add(this.txt_hasta);
+            this.grp_parametros.Controls.Add(this.btn_generar_simulacion);
+            this.grp_parametros.Controls.Add(this.txt_media);
+            this.grp_parametros.Controls.Add(this.lbl_media);
+            this.grp_parametros.Controls.Add(this.txt_cantidad_iteraciones);
+            this.grp_parametros.Controls.Add(this.lbl_cantidad_iteraciones);
+            this.grp_parametros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.grp_parametros.Location = new System.Drawing.Point(12, 12);
+            this.grp_parametros.Name = "grp_parametros";
+            this.grp_parametros.Size = new System.Drawing.Size(1121, 109);
+            this.grp_parametros.TabIndex = 0;
+            this.grp_parametros.TabStop = false;
+            this.grp_parametros.Text = "Parámetros";
+            // 
+            // lbl_desde
+            // 
+            this.lbl_desde.AutoSize = true;
+            this.lbl_desde.Location = new System.Drawing.Point(109, 75);
+            this.lbl_desde.Name = "lbl_desde";
+            this.lbl_desde.Size = new System.Drawing.Size(56, 20);
+            this.lbl_desde.TabIndex = 2;
+            this.lbl_desde.Text = "Desde";
+            // 
+            // lbl_hasta
+            // 
+            this.lbl_hasta.AutoSize = true;
+            this.lbl_hasta.Location = new System.Drawing.Point(399, 72);
+            this.lbl_hasta.Name = "lbl_hasta";
+            this.lbl_hasta.Size = new System.Drawing.Size(52, 20);
+            this.lbl_hasta.TabIndex = 4;
+            this.lbl_hasta.Text = "Hasta";
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(911, 25);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(193, 72);
+            this.btn_limpiar.TabIndex = 7;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // txt_desde
+            // 
+            this.txt_desde.Location = new System.Drawing.Point(171, 69);
+            this.txt_desde.Name = "txt_desde";
+            this.txt_desde.Size = new System.Drawing.Size(200, 26);
+            this.txt_desde.TabIndex = 3;
+            // 
+            // txt_hasta
+            // 
+            this.txt_hasta.Location = new System.Drawing.Point(457, 69);
+            this.txt_hasta.Name = "txt_hasta";
+            this.txt_hasta.Size = new System.Drawing.Size(200, 26);
+            this.txt_hasta.TabIndex = 4;
+            // 
+            // btn_generar_simulacion
+            // 
+            this.btn_generar_simulacion.Location = new System.Drawing.Point(694, 25);
+            this.btn_generar_simulacion.Name = "btn_generar_simulacion";
+            this.btn_generar_simulacion.Size = new System.Drawing.Size(189, 72);
+            this.btn_generar_simulacion.TabIndex = 5;
+            this.btn_generar_simulacion.Text = "Generar Simulación";
+            this.btn_generar_simulacion.UseVisualStyleBackColor = true;
+            this.btn_generar_simulacion.Click += new System.EventHandler(this.btn_generar_simulacion_Click);
+            // 
+            // txt_media
+            // 
+            this.txt_media.Location = new System.Drawing.Point(457, 28);
+            this.txt_media.Name = "txt_media";
+            this.txt_media.Size = new System.Drawing.Size(200, 26);
+            this.txt_media.TabIndex = 2;
+            // 
+            // lbl_media
+            // 
+            this.lbl_media.AutoSize = true;
+            this.lbl_media.Location = new System.Drawing.Point(399, 31);
+            this.lbl_media.Name = "lbl_media";
+            this.lbl_media.Size = new System.Drawing.Size(52, 20);
+            this.lbl_media.TabIndex = 6;
+            this.lbl_media.Text = "Media";
+            // 
+            // txt_cantidad_iteraciones
+            // 
+            this.txt_cantidad_iteraciones.Location = new System.Drawing.Point(171, 28);
+            this.txt_cantidad_iteraciones.Name = "txt_cantidad_iteraciones";
+            this.txt_cantidad_iteraciones.Size = new System.Drawing.Size(200, 26);
+            this.txt_cantidad_iteraciones.TabIndex = 1;
+            // 
+            // lbl_cantidad_iteraciones
+            // 
+            this.lbl_cantidad_iteraciones.AutoSize = true;
+            this.lbl_cantidad_iteraciones.Location = new System.Drawing.Point(14, 31);
+            this.lbl_cantidad_iteraciones.Name = "lbl_cantidad_iteraciones";
+            this.lbl_cantidad_iteraciones.Size = new System.Drawing.Size(156, 20);
+            this.lbl_cantidad_iteraciones.TabIndex = 0;
+            this.lbl_cantidad_iteraciones.Text = "Cantidad Iteraciones";
+            // 
+            // grp_datos
+            // 
+            this.grp_datos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grp_datos.AutoSize = true;
+            this.grp_datos.Controls.Add(this.dgv_resultados);
+            this.grp_datos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.grp_datos.Location = new System.Drawing.Point(12, 127);
+            this.grp_datos.Name = "grp_datos";
+            this.grp_datos.Size = new System.Drawing.Size(1121, 431);
+            this.grp_datos.TabIndex = 1;
+            this.grp_datos.TabStop = false;
+            this.grp_datos.Text = "Resultados iteración desde - hasta";
+            // 
+            // dgv_resultados
+            // 
+            this.dgv_resultados.AllowUserToAddRows = false;
+            this.dgv_resultados.AllowUserToDeleteRows = false;
+            this.dgv_resultados.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv_resultados.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dgv_resultados.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgv_resultados.BackgroundColor = System.Drawing.SystemColors.ScrollBar;
+            this.dgv_resultados.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dgv_resultados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_resultados.Location = new System.Drawing.Point(18, 25);
+            this.dgv_resultados.Name = "dgv_resultados";
+            this.dgv_resultados.ReadOnly = true;
+            this.dgv_resultados.RowHeadersWidth = 30;
+            this.dgv_resultados.RowTemplate.Height = 25;
+            this.dgv_resultados.Size = new System.Drawing.Size(1086, 388);
+            this.dgv_resultados.TabIndex = 0;
+            // 
+            // grp_resultados
+            // 
+            this.grp_resultados.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grp_resultados.AutoSize = true;
+            this.grp_resultados.Controls.Add(this.txt_toneladas_totales);
+            this.grp_resultados.Controls.Add(this.lbl_resultado);
+            this.grp_resultados.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.grp_resultados.Location = new System.Drawing.Point(12, 564);
+            this.grp_resultados.Name = "grp_resultados";
+            this.grp_resultados.Size = new System.Drawing.Size(1121, 89);
+            this.grp_resultados.TabIndex = 2;
+            this.grp_resultados.TabStop = false;
+            this.grp_resultados.Text = "Resultados";
+            // 
+            // txt_toneladas_totales
+            // 
+            this.txt_toneladas_totales.Enabled = false;
+            this.txt_toneladas_totales.Location = new System.Drawing.Point(377, 38);
+            this.txt_toneladas_totales.Name = "txt_toneladas_totales";
+            this.txt_toneladas_totales.Size = new System.Drawing.Size(466, 26);
+            this.txt_toneladas_totales.TabIndex = 1;
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.AutoSize = true;
+            this.lbl_resultado.Location = new System.Drawing.Point(232, 44);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(139, 20);
+            this.lbl_resultado.TabIndex = 0;
+            this.lbl_resultado.Text = "Toneladas Totales";
+            // 
+            // TestMontecarlo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 663);
+            this.Controls.Add(this.grp_resultados);
+            this.Controls.Add(this.grp_datos);
+            this.Controls.Add(this.grp_parametros);
+            this.Name = "TestMontecarlo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TP4 - Simulación de Montecarlo";
+            this.grp_parametros.ResumeLayout(false);
+            this.grp_parametros.PerformLayout();
+            this.grp_datos.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_resultados)).EndInit();
+            this.grp_resultados.ResumeLayout(false);
+            this.grp_resultados.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grp_parametros;
+        private System.Windows.Forms.Label lbl_cantidad_iteraciones;
+        private System.Windows.Forms.Label lbl_desde;
+        private System.Windows.Forms.TextBox txt_hasta;
+        private System.Windows.Forms.Label lbl_hasta;
+        private System.Windows.Forms.TextBox txt_desde;
+        private System.Windows.Forms.GroupBox grp_datos;
+        private System.Windows.Forms.GroupBox grp_resultados;
+        private System.Windows.Forms.TextBox txt_media;
+        private System.Windows.Forms.Label lbl_media;
+        private System.Windows.Forms.Button btn_generar_simulacion;
+        private System.Windows.Forms.DataGridView dgv_resultados;
+        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.TextBox txt_toneladas_totales;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.TextBox txt_cantidad_iteraciones;
+    }
+}
+
---
+++ TP4_SIM/TestMontecarlo.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TP4_SIM_vLihuen
+{
+    public partial class TestMontecarlo : Form
+    {
+        public TestMontecarlo()
+        {
+            InitializeComponent();
+        }
+        NumberFormatInfo formato = new CultureInfo("es-AR").NumberFormat;
+        NumberFormatInfo formato2 = new CultureInfo("es-AR").NumberFormat;
+
+        private void btn_generar_simulacion_Click(object sender, EventArgs e)
+        {
+            int cantidad_iteraciones = int.Parse(txt_cantidad_iteraciones.Text);
+            int iteracion_desde = int.Parse(txt_desde.Text);
+            int iteracion_hasta = int.Parse(txt_hasta.Text);
+            double media = float.Parse(txt_media.Text);
+
+            formato.CurrencyGroupSeparator = ".";
+            formato.NumberDecimalSeparator = ",";
+            formato.NumberDecimalDigits = 4;
+
+            if (GenerarSimulacion(cantidad_iteraciones, iteracion_desde, iteracion_hasta, media, out DataTable simulacion_desde_hasta, out double produccion_total))
+            {
+                dgv_resultados.DataSource = simulacion_desde_hasta;
+                txt_toneladas_totales.Text = produccion_total.ToString("N",formato);
+            }
+            else
+            {
+                // Lanzar evento error en ventana
+            }
+        }
+
+        private bool GenerarSimulacion(int iteraciones, int desde, int hasta, double media_exp, out DataTable simulacion_desde_hasta, out double produccion_total)
+        {
+            Stopwatch timeMeasure = new();
+
+            int cantidad_iteraciones = iteraciones;
+            int iteracion_desde = desde;
+            int iteracion_hasta = hasta;
+            double media = media_exp;
+
+            formato.CurrencyGroupSeparator = ".";
+            formato.NumberDecimalSeparator = ",";
+            formato.NumberDecimalDigits = 4;
+
+            formato2.CurrencyGroupSeparator = ".";
+            formato2.NumberDecimalSeparator = ",";
+            formato2.NumberDecimalDigits = 0;
+
+            DataTable resultado = new();
+            resultado.Columns.Add("Dia");
+            resultado.Columns.Add("RND Fertilizante");
+            resultado.Columns.Add("Llegada fertilizante (días)");
+            resultado.Columns.Add("RND Clima");
+            resultado.Columns.Add("Clima");
+            resultado.Columns.Add("Sol");
+            resultado.Columns.Add("Lluvia");
+            resultado.Columns.Add("Nublado");
+            resultado.Columns.Add("Prob Sol");
+            resultado.Columns.Add("Prob Lluvia");
+            resultado.Columns.Add("Prob Nublado");
+            resultado.Columns.Add("Produccion (Toneladas)");
+
+            double random_fertilizante = 0, llegada_fertilizante = 0, random_clima, produccion_ayer = 0, produccion_hoy = 0;
+
+            int num_clima_ayer = 0; // 0 para el calculo del primer clima, 1 Sol - 2 Lluvia - 3 Nublado
+            int num_clima_hoy; // 1 Sol - 2 Lluvia - 3 Nublado
+
+            // Contadores, pos 0 (sol) pos 1 (lluvia) pos 2 (nublado)
+            int[] contador_climas = { 0, 0, 0 };
+
+            // Acumulador probabilidades climas, pos 0 (sol) pos 1 (lluvia) pos 2 (nublado)
+            double[] acumulador_prob_climas = { 0.0, 0.0, 0.0 };
+
+            // Arreglo muldimensional de la probabilida de los climas
+            // pos 0 para el primer día, pos 1 (sol) pos 2 (lluvia) pos 3 (nublado)
+            double[][] probabilidades_clima =
+            {
+                new double[] { 0.333, 0.667, 1.0 },
+                new double [] { 0.80, 0.90, 1.00 },
+                new double[] { 0.40, 0.80, 1.00 },
+                new double [] { 0.60, 0.70, 1.00 }
+            };
+
+            Random rnd = new();
+            random_clima = Math.Truncate(rnd.NextDouble() * 10000) / 10000;
+
+            num_clima_ayer = DeterminarClima(random_clima, num_clima_ayer, probabilidades_clima);
+
+            // Iniciamos el reloj
+            timeMeasure.Start();
+
+            for (int i = 1; i < cantidad_iteraciones + 1; i++)
+            {
+                // Obtengo randoms para fertilizante (cada 10 días)
+                if ((i - 1) % 10 == 0)
+                {
+                    random_fertilizante = Math.Truncate(rnd.NextDouble() * 10000) / 10000;
+                    llegada_fertilizante = Math.Truncate(CanlcularLlegadaFertilizante(media, random_fertilizante) * 10000) / 10000;
+                }
+
+                // Obtengo el clima de hoy en base al día anterior
+                random_clima = Math.Truncate(rnd.NextDouble() * 10000) / 10000;
+                num_clima_hoy = DeterminarClima(random_clima, num_clima_ayer, probabilidades_clima);
+
+                // Sumo contador de climas
+                contador_climas[num_clima_hoy - 1]++;
+
+                // Calculo probabilidad acumulada de climas
+                acumulador_prob_climas[0] = Math.Truncate(((double)contador_climas[0] / i) * 10000) / 10000;
+                acumulador_prob_climas[1] = Math.Truncate(((double)contador_climas[1] / i) * 10000) / 10000;
+                acumulador_prob_climas[2] = Math.Truncate(((double)contador_climas[2] / i) * 10000) / 10000;
+
+                produccion_hoy = Math.Truncate(((3350 + 250 * acumulador_prob_climas[0] + 390 * acumulador_prob_climas[1] - 150 * acumulador_prob_climas[2] - 600 * llegada_fertilizante) + produccion_ayer) * 10000) / 10000;
+
+                if (i >= iteracion_desde && i <= iteracion_hasta || i == cantidad_iteraciones)
+                {
+                    string clima = ObtenerClima(num_clima_hoy);
+                    resultado.Rows.Add(i.ToString("N",formato2), random_fertilizante, llegada_fertilizante,
+                                                    random_clima, clima, contador_climas[0],
+                                                    contador_climas[1], contador_climas[2],
+                                                    acumulador_prob_climas[0],
+                                                    acumulador_prob_climas[1],
+                                                    acumulador_prob_climas[2],
+                                                    produccion_hoy.ToString("N", formato));
+                }
+
+                num_clima_ayer = num_clima_hoy;
+                contador_climas[0] = 0;
+                contador_climas[1] = 0;
+                contador_climas[2] = 0;
+                produccion_ayer = produccion_hoy;
+            }
+
+            //Paramos el reloj
+            timeMeasure.Stop();
+            Debug.WriteLine($"Tiempo for: {timeMeasure.Elapsed.TotalSeconds} s");
+
+            simulacion_desde_hasta = resultado;
+            produccion_total = produccion_hoy;
+            return true;
+        }
+
+        private static double CanlcularLlegadaFertilizante(double media, double random_fertilizante)
+        {
+            return Math.Truncate(-media * Math.Log(1 - random_fertilizante) * 10000) / 10000;
+        }
+
+        private static int DeterminarClima(double random_clima, int num_clima_ayer, double[][] probabilidades_clima)
+        {
+            if (random_clima < probabilidades_clima[num_clima_ayer][0]) return 1;
+            if (random_clima < probabilidades_clima[num_clima_ayer][1]) return 2;
+            return 3;
+        }
+
+        private static string ObtenerClima(int num_clima)
+        {
+            if (num_clima - 1 == 0) return "Sol";
+            if (num_clima - 1 == 1) return "Lluvia";
+            return "Nublado";
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            dgv_resultados.DataSource = null;
+            txt_cantidad_iteraciones.Clear();
+            txt_desde.Clear();
+            txt_hasta.Clear();
+            txt_media.Clear();
+            txt_toneladas_totales.Clear();
+        }
+    }
+}
